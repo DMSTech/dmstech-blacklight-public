7651db74568d39dd07c1fce41a31bd910fddd2e6
Chris Beer <cabeer@stanford.edu>
2013-09-23 10:47:08 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917202813) do
+ActiveRecord::Schema.define(version: 20130917222259) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",     null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20130917202813) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "guest",                  default: false
+    t.integer  "parker_image_access"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

d2cd7ab9ea280ceacdee81a69f295246827dedfe
blalbritton <blalbrit@stanford.edu>
2014-10-05 13:53:15 -0700

    adding IIIF thumbs back in - requires auth for functionality for Parker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     thumbnail = nil
     if manuscript_number and PARKER_MASTER.has_key?(manuscript_number)
       druid = PARKER_MASTER[manuscript_number]['druid']
-      thumbnail = image_tag get_preview_image(document, druid, image_options[:size]), :alt => 'Manuscript preview', :class => 'thumbImg', :title => preview_image_url
+      thumbnail = image_tag get_preview_image(document, druid, image_options[:size]), :alt => 'Manuscript preview', :class => 'thumbImg', :title => get_preview_image(document, druid, image_options[:size])
     end
 
     # check if manuscript belongs to e-codices collection and get thumbnail

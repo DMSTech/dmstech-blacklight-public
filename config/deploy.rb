1f5a1900e8e8341607ce743db3b427d49ca957bd
Chris Beer <cabeer@stanford.edu>
2015-07-01 13:40:55 -0700

    ./bin should not be a shared directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :linked_files, %w{
 }
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

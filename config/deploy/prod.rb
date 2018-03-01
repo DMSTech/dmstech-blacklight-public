42c1a54d55e24cad398475056b29f22ebe04f8a3
Chris Beer <cabeer@stanford.edu>
2018-03-01 15:32:54 -0800

    Make the application deployable

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,6 @@
 server 'sul-dms-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
+set :rails_env, 'production'
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
